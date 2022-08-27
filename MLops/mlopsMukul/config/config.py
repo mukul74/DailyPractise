d99411f4a347c7dfbe7ed1d99a03e5ccbca82df8
mukul74 <agarwal1993mukul@gmail.com>
2022-08-27 14:26:28 +0200

    setup procedure for the project

--- MLops/mlopsMukul/config/config.py
+++ MLops/mlopsMukul/config/config.py
@@ -1,6 +1,12 @@
-# config.py
+# config/config.py
 from pathlib import Path
+import pretty_errors
 
 # Directories
 BASE_DIR = Path(__file__).parent.parent.absolute()
 CONFIG_DIR = Path(BASE_DIR, "config")
+DATA_DIR = Path(BASE_DIR, "data")
+
+# Create dirs
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+
